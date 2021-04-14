c464925e6ec29485459e474fcbd69cd9783678e5
Khaos <okh8609@gmail.com>
2021-04-14 23:23:21 +0800

    ansible OK

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "[server]" > inventory
+
+while IFS=" " read -r host _ port 
+do
+	echo "$host ansible_port=$port anisible_user=root" >> inventory  
+done < ip.txt
+
+echo "[server:vars]" >> inventory
+echo "file_path=/home/kh/iot_project2_CI/rpi/" >> inventory # the path to working directory
+
+while read line; do
+	ssh-copy-id $line
+done < ip.txt
+
+ansible-playbook playbook.yml
---
+++ rpi/blink_new.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+import RPi.GPIO as GPIO
+import time
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(11,GPIO.OUT)
+    print("OK")
+
+def blink():
+    while True:
+        GPIO.output(11,GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(11,GPIO.LOW)
+        time.sleep(0.5)
+
+if __name__=="__main__":
+    setup()
+    blink()
