c5612e84b4ad09f7ef136c3d49dbd0cf0d3ae5b7
Khaos <okh8609@gmail.com>
2021-04-15 16:38:43 +0800

    .

--- rpi/iot_run.py
+++ rpi/iot_run.py
@@ -7,16 +7,16 @@ def setup():
     GPIO.setup(11,GPIO.OUT)
     print("OK")
 
-def blin
-    while Tru
-        GPIO.output(11,GPIO.HIGH)
-        time.sleep(1.5)
-        GPIO.output(11,GPIO.LOW)
-        time.sleep(1.5)
+def blink():
+    while True:
         # GPIO.output(11,GPIO.HIGH)
-        # time.sleep(0.08)
+        # time.sleep(1.5)
         # GPIO.output(11,GPIO.LOW)
-        # time.sleep(0.08)
+        # time.sleep(1.5)
+        GPIO.output(11,GPIO.HIGH)
+        time.sleep(0.08)
+        GPIO.output(11,GPIO.LOW)
+        time.sleep(0.08)
 
 if __name__=="__main__":
     setup()
